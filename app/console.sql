6ba997ae8d75b06e266f7ff264377bfa71f53d06
RhandyPiedadMartinez <mrpmartinez@tip.edu.ph>
2022-04-08 14:13:45 +0800

    Updated mysql codes
    Change-Id: I33f9e191b576350efde60d1a4c8c212d853eee33
    Signed-off-by: RhandyPiedadMartinez <mrpmartinez@tip.edu.ph>

--- app/console.sql
+++ app/console.sql
@@ -3,40 +3,22 @@ CREATE TABLE persons (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `first_name` VARCHAR(64),
     `last_name` VARCHAR(64),
-    `sex` ENUM('male','female'),
-    `birthdate` DATE,
-    `address` VARCHAR(128),
     `email` VARCHAR(256),
     `sha1_password` VARCHAR(40),
-    `phone_number` VARCHAR(32),
-    `parent_id` INT,
+    `e_wallet` INT(11),
+    `spotify_bill` INT(11),
+    `discord_nitro_bill` INT(11),
+    `ph_premium_bill` INT(11),
+    
     UNIQUE (`first_name`,`last_name`),
-    UNIQUE(`email`),
-    FOREIGN KEY (parent_id)
-        REFERENCES persons(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
+    UNIQUE(`email`)
     );
 
-ALTER TABLE person
-    ADD COLUMN `blood_type`
-    ENUM('A+','A-','B+','B-','O+','O-','AB');
+	INSERT INTO persons(`first_name`,`last_name`,`email`,`sha1_password`,`e_wallet`,`spotify_bill`,`discord_nitro_bill`,`ph_premium_bill`)
+    VALUES('Clark','Testerson','@ereh',SHA1('BirthdayCake'),999999,100,500,630);
 
-INSERT INTO persons(`birthdate`,`first_name`,`last_name`,`sex`)
-    VALUES('1990-03-05','Clark','Testerson','Male');
+   INSERT INTO persons(`first_name`,`last_name`,`email`,`sha1_password`,`e_wallet`,`spotify_bill`,`discord_nitro_bill`,`ph_premium_bill`)
+    VALUES('Heda','Testerson','@bertoroto',SHA1('BirthdayParty'),999999,100,600,610);
 
-INSERT INTO persons(`birthdate`,`first_name`,`last_name`,`sex`)
-VALUES('1990-03-05','Heda','Testerson','Male');
-
-INSERT INTO persons(`birthdate`,`first_name`,`last_name`,`sex`)
-VALUES('1990-03-05','Rhandy','Testerson','Male');
-
-UPDATE `persons`
-SET email = 'pedro.testerson@gmail.com'
-WHERE  first_name = 'Pedro' AND `last_name` = 'Testerson'
-LIMIT 1;
-
-UPDATE `persons`
-SET sha1_password = SHA1('BirthdayCake')
-WHERE  first_name = 'Pedro' AND `last_name` = 'Testerson'
-LIMIT 1;
+    INSERT INTO persons(`first_name`,`last_name`,`email`,`sha1_password`,`e_wallet`,`spotify_bill`,`discord_nitro_bill`,`ph_premium_bill`)
+    VALUES('Rhandy','Martinez','@armin',SHA1('Funeral'),9999999,1020,700,600);
