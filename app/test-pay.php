cbd1e522cdcf618931110bda1a2fd33d91f9c3c3
RhandyPiedadMartinez <mrpmartinez@tip.edu.ph>
2022-04-11 10:26:20 +0800

    wala lang HAHAHA
    Change-Id: I7a98ce47ef7c771f1893fcbc2b02f64f66272f2f
    Signed-off-by: RhandyPiedadMartinez <mrpmartinez@tip.edu.ph>

--- app/test-pay.php
+++ app/test-pay.php
@@ -5,7 +5,8 @@ $current_user = $_SESSION["login"];
 include ("config.php");
 
 if (isset($_POST['spotify'])) {
-    $sql = "UPDATE persons SET e_wallet=e_wallet-spotify_bill, spotify_bill=spotify_bill-spotify_bill WHERE email='$current_user'";
+    $sql = "SELECT spotify_bill IF(e_wallet>spotify_bill, 'Payable', 'Lack') where email='$current_user' FROM persons"; 
+    
 } elseif (isset($_POST['discord'])) {
 
     $sql = "UPDATE persons SET e_wallet=e_wallet-discord_nitro_bill, discord_nitro_bill=discord_nitro_bill-discord_nitro_bill WHERE email='$current_user'";
