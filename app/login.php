2ff5b8400a2ec2ffacf535d5d2c16ca4ea312fdf
Clark <daprogrammer1@gmail.com>
2022-04-12 17:35:38 +0800

    add tailwind

--- app/login.php
+++ app/login.php
@@ -13,6 +13,7 @@
 <script
 	src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 
+<script src="https://cdn.tailwindcss.com"></script>
 
 <script
 	src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
@@ -32,110 +33,41 @@
 	integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
 	crossorigin="anonymous">
 
-<style>
-body {
-	font-family: Arial, Helvetica, sans-serif;
-}
+<link rel="stylesheet" href="./static/css/login.css">
 
-* {
-	box-sizing: border-box;
-}
-
-body, html {
-    padding-top:40px;
-	scroll-behavior: smooth;
-	height: 100%;
-	background: #3AAFA9;
-	font-family: monospace;
-	color: #FEFFFF;
-	/* For browsers that do not support gradients */
-	background-image: linear-gradient(270deg, #172529, 42%, #3AAFA9);
-	color: #FEFFFF;
-}
-
-/* Add styles to the form container */
-.form-container {
-	max-width: 400px;
-	padding: 10px;
-	background-color: #172529;
-	border-radius: 10px;
-	font-weight: bold;
-	border-style: groove ;
-	border-color: #FEFFFF;
-}
-
-/* Full-width input fields */
-.form-container input[type=text], .form-container input[type=password] {
-	width: 100%;
-	padding: 15px;
-	margin: 5px 0 22px 0;
-	border: none;
-	background: #f1f1f1;
-	border-radius: 10px;
-}
-
-/* When the inputs get focus, do something */
-.form-container input[type=text]:focus, .form-container input[type=password]:focus
-	{
-	background-color: #ddd;
-	outline: none;
-}
-
-/* Set a style for the submit/login button */
-.form-container .btn {
-	max-width: 100%;
-	background-color: #3AAFA9;
-	color: white;
-	padding: 16px 20px;
-	border: none;
-	cursor: pointer;
-	width: 100%;
-	opacity: 0.8;
-	border-radius: 10px;
-}
-
-/* Add some hover effects to buttons */
-.form-container .btn:hover, .open-button:hover {
-	opacity: 1;
-}
-/*
-		color pallette
-		background: #172529;
-		background: #2B7A78;
-		background: #3AAFA9;
-		background: #DEF2F1;
-		background: #FEFFFF;
-		*/
-.center {
-	display: flex;
-	justify-content: center;
-}
-</style>
 </head>
-<body>
+<body class="pt-20">
 	<div>
 		<div class="center">
-			<span style="font-size: 90px; font-family: serif;"> <b>Pay</b>oda
+			<span style="font-family: serif;" class="text-7xl mb-12"> 
+				<b>Pay</b>oda
 			</span>
 		</div>
 		<div class="center">
-			<form action="loginprocess.php" method="POST" class="form-container">
+			<form action="loginprocess.php" method="POST" class="form-container rounded-xl font-weight-bold p-4">
 
-				<h1 style="font-weight: bold;">Login</h1>
+				<div style class="fw-bold">Login</div>
 
-				<label for="email"><b>Email</b></label> <input type="text"
-					placeholder="Enter Email" name="uname" required=""> <label
-					for="psw"><b>Password</b></label> <input type="password"
-					placeholder="Enter Password" name="upassword" required="">
+				<label for="email">
+					<b>Email</b>
+				</label> 
+				<input type="text"
+					placeholder="Enter Email" name="uname" required> 
+				<label
+					for="psw">
+					<b>Password</b>
+				</label> 
+				<input type="password"
+					placeholder="Enter Password" name="upassword" required>
 
 				<button type="submit" value="Login" name="sub" class="btn">Login</button>
 
-				<label style="padding-top: 10px;margin-left: 50px;">
+				<label class="ml-12 pt-3">
 				     <?php
                 if (isset($_REQUEST["err"]))
                     $msg = "Invalid username or Password";
                 ?>
-                <h5 style="color: red;">
+                <h5 class="text-red-600">
                 <?php
 
                 if (isset($msg)) {
@@ -151,16 +83,5 @@ body, html {
 		<br>
 	</div>
 	</div>
-
-
-
-
-
-
-
-
-
-
-
 </body>
 </html>
