de565123b50eeb93c453f1e359b18f029c20d684
RhandyPiedadMartinez <mrpmartinez@tip.edu.ph>
2022-04-15 08:38:36 +0800

    access localhost/setdb.php
    Change-Id: Ib1001b65935e394f3965a9bc2e54e1de0d3aa732
    Signed-off-by: RhandyPiedadMartinez <mrpmartinez@tip.edu.ph>

--- app/console.sql
+++ app/console.sql
@@ -16,11 +16,11 @@ CREATE TABLE `persons` (
 
 
 INSERT INTO `persons` (`id`, `first_name`, `last_name`, `email`, `sha1_password`, `e_wallet`, `spotify_bill`, `discord_nitro_bill`, `ph_premium_bill`, `netflix_bill`, `yt_premium_bill`, `profile_pic`) VALUES
-(1, 'Clark', 'Testerson', 'clark', 'a9993e364706816aba3e25717850c26c9cd0d89d', 0, 0, 0, 0, 1, 1, 'https://i1.wp.com/9tailedkitsune.com/wp-content/uploads/2020/12/letsycakes_mikasa.jpeg?ssl=1'),
-(2, 'Heda', 'Doe', 'heda', 'a9993e364706816aba3e25717850c26c9cd0d89d', 1000, -1, 500, -1, 1, 1, 'https://i.pinimg.com/originals/04/28/2e/04282e49895b2287ccd3d4bdb7d334b9.png'),
-(3, 'Rhandy', 'Martinez', 'rhandy', 'a9993e364706816aba3e25717850c26c9cd0d89d', 1598, 0, 0, 0, 0, 0, 'https://i.pinimg.com/originals/9c/4a/22/9c4a228a30e0be34ed9fdbb7a4e43143.jpg'),
-(4, 'Roldam', 'Martinez', 'abc', 'a9993e364706816aba3e25717850c26c9cd0d89d', 0, 0, 0, 0, 1, 1, 'https://i.pinimg.com/564x/6c/3f/e4/6c3fe4350219e3eb665fc5a5ad95cc65.jpg'),
-(5, 'Ronie', 'Martinez', 'asd', 'a9993e364706816aba3e25717850c26c9cd0d89d', 500, -1, -1, -1, 1, 1, 'https://i.pinimg.com/564x/68/b0/f1/68b0f156020a20c7ff9cc3b0dd4390c9.jpg');
+(1, 'Clark', 'Testerson', 'clark', 'a9993e364706816aba3e25717850c26c9cd0d89d', 990, 909, 990, 199, -1, -1, 'https://i1.wp.com/9tailedkitsune.com/wp-content/uploads/2020/12/letsycakes_mikasa.jpeg?ssl=1'),
+(2, 'Heda', 'Doe', 'heda', 'a9993e364706816aba3e25717850c26c9cd0d89d', 1000, -1, 500, -1, 19, 19, 'https://i.pinimg.com/originals/04/28/2e/04282e49895b2287ccd3d4bdb7d334b9.png'),
+(3, 'Rhandy', 'Martinez', 'rhandy', 'a9993e364706816aba3e25717850c26c9cd0d89d', 1598, 10, 10, 10, 110, -1, 'https://i.pinimg.com/originals/9c/4a/22/9c4a228a30e0be34ed9fdbb7a4e43143.jpg'),
+(4, 'Roldam', 'Martinez', 'abc', 'a9993e364706816aba3e25717850c26c9cd0d89d', 20,90, 990, 10, 19, 199, 'https://i.pinimg.com/564x/6c/3f/e4/6c3fe4350219e3eb665fc5a5ad95cc65.jpg'),
+(5, 'Ronie', 'Martinez', 'asd', 'a9993e364706816aba3e25717850c26c9cd0d89d', 500, -1, -1, -1, 919, -1, 'https://i.pinimg.com/564x/68/b0/f1/68b0f156020a20c7ff9cc3b0dd4390c9.jpg');
 
 
 CREATE TABLE `biller_list` (
