fe8a4dc5217a36e10641f443123f48374e6769d0
Clark <daprogrammer1@gmail.com>
2022-04-19 06:48:15 +0800

    changes

--- app/index.php
+++ app/index.php
@@ -35,10 +35,8 @@ if ($result = mysqli_query($mysqli, $sql)) {
 <html lang="en">
 <head>
 <!-- Bootstrap -->
-<link rel="stylesheet"
-	href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-<script
-	src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+<script	src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- Tailwind -->
 <script src="https://cdn.tailwindcss.com"></script>
